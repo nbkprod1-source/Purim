f279a9e90b2d76e86d71fd2ed1e85460b1062677
nbkprod1-source <nbk.prod1@gmail.com>
2026-02-11 14:32:55 +0200

    Add files via upload

--- pages/2_Scoreboard.py
+++ pages/2_Scoreboard.py
@@ -44,7 +44,8 @@ st.markdown("""
     
     .block-container { padding-top: 2rem !important; padding-bottom: 5rem !important; }
     #MainMenu, footer, header { visibility: hidden; }
-/* הסתרת התפריט הצדדי לחלוטין */
+    
+    /* הסתרת התפריט הצדדי לחלוטין */
     [data-testid="stSidebar"] { display: none !important; }
     [data-testid="stSidebarNav"] { display: none !important; }
     [data-testid="collapsedControl"] { display: none !important; }
